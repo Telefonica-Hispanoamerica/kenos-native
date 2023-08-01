371c466f71564d9e29036b15a338db2af0f0c77e
Cancela <nicolas.cancela@getronics.com>
2023-08-01 12:14:19 -0300

    Added sheet.

---
+++ src/components/Sheet/Sheet.tsx
@@ -0,0 +1,131 @@
+import React, {useRef, useEffect} from 'react';
+import {
+  View,
+  Animated,
+  StyleSheet,
+  TouchableOpacity,
+  PanResponder,
+  Dimensions,
+  ScrollView,
+  GestureResponderEvent,
+  PanResponderGestureState,
+} from 'react-native';
+import {useTheme} from '../../hooks/ThemeContextProvider';
+
+interface BottomSheetProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const BottomSheet = ({isOpen, onClose, children}: BottomSheetProps) => {
+  const {height} = Dimensions.get('window');
+  const translateY = useRef(new Animated.Value(isOpen ? 0 : height)).current;
+  const {skin} = useTheme();
+  const {background, control} = skin.colors;
+
+  const styles = StyleSheet.create({
+    overlay: {
+      ...StyleSheet.absoluteFillObject,
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      justifyContent: 'flex-end',
+    },
+    bottomSheet: {
+      backgroundColor: background,
+      borderTopLeftRadius: 10,
+      borderTopRightRadius: 10,
+      minHeight: 200, // Altura mínima del Sheet.
+      maxHeight: Dimensions.get('window').height * 0.7, //La altura máxima no puede superar el 70% de la pantalla.
+    },
+    draggableHandle: {
+      height: 5,
+      width: 40,
+      backgroundColor: control,
+      alignSelf: 'center',
+      marginVertical: 10,
+      borderRadius: 5,
+    },
+    scrollContainer: {
+      flexGrow: 1,
+    },
+  });
+
+  useEffect(() => {
+    if (isOpen) {
+      handleOpen();
+    } else {
+      handleClose();
+    }
+  }, [isOpen]);
+
+  const handleOpen = () => {
+    Animated.timing(translateY, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const handleClose = () => {
+    Animated.timing(translateY, {
+      toValue: height,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(onClose);
+  };
+
+  const handlePanResponderMove = (
+    _: GestureResponderEvent,
+    gestureState: PanResponderGestureState,
+  ) => {
+    translateY.setValue(Math.min(Math.max(gestureState.dy, 0), height - 100));
+  };
+
+  const handlePanResponderRelease = (
+    _: GestureResponderEvent,
+    gestureState: PanResponderGestureState,
+  ) => {
+    if (gestureState.dy > height * 0.2) {
+      // Si arrastramos más del 20% de la pantalla hacia abajo, ocultamos el Sheet.
+      handleClose();
+    } else {
+      handleOpen();
+    }
+  };
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: handlePanResponderMove,
+      onPanResponderRelease: handlePanResponderRelease,
+    }),
+  ).current;
+
+  const bottomSheetStyle = {
+    transform: [{translateY}],
+  };
+
+  return (
+    <>
+      {isOpen && (
+        <TouchableOpacity
+          activeOpacity={1}
+          style={styles.overlay}
+          onPress={handleClose}>
+          <Animated.View style={[styles.bottomSheet, bottomSheetStyle]}>
+            <View
+              style={styles.draggableHandle}
+              {...panResponder.panHandlers}
+            />
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+              {children}
+            </ScrollView>
+          </Animated.View>
+        </TouchableOpacity>
+      )}
+    </>
+  );
+};
+
+export default BottomSheet;
