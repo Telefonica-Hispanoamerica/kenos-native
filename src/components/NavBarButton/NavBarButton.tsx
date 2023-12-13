dd140a250f5b09fb02660c6d24845b97aab9ebad
lucasnahuelacuna <lucas.nahuel.acuna@gmail.com>
2023-12-13 15:57:58 -0300

    Fix NavBar stories warnings imports

---
+++ src/components/NavBarButton/NavBarButton.tsx
@@ -0,0 +1,36 @@
+import {GestureResponderEvent, Pressable, View, StyleSheet} from 'react-native';
+import { isIos } from '../../utils';
+
+type NavBarButtonProps = {
+  onPress: (event: GestureResponderEvent) => void;
+  children: JSX.Element;
+};
+
+function NavBarButton({onPress, children}: NavBarButtonProps) {
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable
+        onPress={onPress}
+        style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+        android_ripple={{color: isIos() ? 'white' : 'rgba(255,255,255,0.4)', foreground: true}}>
+        {children}
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+      height: isIos() ? 24 : 28, 
+      width: isIos() ? 24 : 28, 
+      borderRadius: 50, 
+      overflow: 'hidden'
+    },
+    button: {
+      flex: 1, 
+      justifyContent: 'center', 
+      alignItems: 'center'
+    }
+  });
+
+export default NavBarButton;
