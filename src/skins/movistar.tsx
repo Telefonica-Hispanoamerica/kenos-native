2d3e66f760fd7483db8340cf456b4948c5dffa89
Sergio Rubilar <sergio.rubilar@telefonica.com>
2024-02-12 19:48:45 -0300

    fix token color

--- src/skins/movistar.tsx
+++ src/skins/movistar.tsx
@@ -37,7 +37,7 @@ export const palette = {
   grey5: '#86888C',
   grey6: '#313235',
   white: '#FFFFFF',
-  movistarBlueDark: '#313235',
+  movistarBlueDark: '#0B2739',
   darkModeBlack: '#061824',
   darkModeGrey: '#081F2E',
   darkModeGrey2: '#EAEBEE',
