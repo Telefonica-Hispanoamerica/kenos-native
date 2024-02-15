dfaeb4cc2724f532dcb2f4b1cb13d97c59416ec5
Sergio Rubilar <sergio.rubilar@telefonica.com>
2024-02-15 19:14:18 -0300

    Add new functions for Tabs and Tabs Stories

--- src/__stories__/Tabs.stories.tsx
+++ src/__stories__/Tabs.stories.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import Tabs from '../components/Tabs/Tabs';
 import { View } from 'react-native';
@@ -7,9 +8,17 @@ export default {
     component: Tabs,
 } as ComponentMeta<typeof Tabs>
 
-export const Basic: ComponentStory<typeof Tabs> = ( args ) => (
-    <View>
-    </View>
-)
+export const Basic: ComponentStory<typeof Tabs> = (args) => {
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+    return (
+        <View>
+            <Tabs selectedIndex={selectedIndex} onChange={setSelectedIndex}
+            tabs={['First Tab', 'Second Tab', 'Third Tab'].map((text) => ({
+                text,
+            }))}
+            ></Tabs>
+        </View>
+    )
+}
 
 Basic.storyName = 'Tabs';
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import { View } from 'react-native';
-import { useElementDimensions } from '../../hooks/hooks'
+import React, { useRef, useState } from 'react';
+import { ScrollView, Text, TouchableWithoutFeedback, View } from 'react-native';
 import { DataAttributes } from '../../utils/types';
-import styles from './Tabs.css';
+import * as styles from './Tabs.css';
 
-const LINE_ANIMATION_DURATION_MS =  300;
+const LINE_ANIMATION_DURATION_MS = 300;
 
-export type TabsProps = {
+const getTabVariant = (numberOfTabs: number): keyof typeof styles.tabVariantStyles => {
+    switch (numberOfTabs) {
+        case 2:
+            return 'tabs2';
+        case 3:
+            return 'tabs3';
+        default:
+            return 'default';
+    }
+};
+
+type TabsProps = {
     selectedIndex: number;
     onChange: (selectedIndex: number) => void;
     tabs: ReadonlyArray<{
@@ -18,12 +28,55 @@ export type TabsProps = {
     dataAttributes?: DataAttributes;
 };
 
-const Tabs: React.FC<TabsProps> = ({selectedIndex, onChange, tabs, dataAttributes }: TabsProps) => {
-    
+const Tabs: React.FC<TabsProps> = ({ selectedIndex, onChange, tabs, dataAttributes }: TabsProps) => {
+    const animatedLineRef = useRef<View>(null);
+    const scrollableContainerRef = useRef<ScrollView>(null);
+    const [isAnimating, setIsAnimating] = useState(false);
+
+    const animateLine = (fromIndex: number, toIndex: number) => {
+        // Implementa la lógica para animar la línea
+    };
+
     return (
-        <>
-        </>
+        <View> 
+            <ScrollView horizontal>
+                <View>
+                    <View> 
+                        <View style={styles.outerStyles}>
+                            <ScrollView
+                                horizontal
+                                showsHorizontalScrollIndicator={false}
+                                ref={scrollableContainerRef}
+                            >
+                                <View style={styles.tabStyles.tabsContainer}>
+                                    {tabs.map(({ text, icon, 'aria-controls': ariaControls }, index) => (
+                                        <TouchableWithoutFeedback
+                                            key={index}
+                                            onPress={() => {
+                                                if (!isAnimating && selectedIndex !== index) {
+                                                    onChange(index);
+                                                    animateLine(selectedIndex, index);
+                                                }
+                                            }}
+                                            disabled={isAnimating || selectedIndex === index}
+                                        >
+                                            <View
+
+                                            >
+                                                {icon && <View style={styles.tabStyles.icon}>{icon}</View>}
+                                                <Text>{text}</Text>
+                                            </View>
+                                        </TouchableWithoutFeedback>
+                                    ))}
+                                </View>
+                            </ScrollView>
+                            <View ref={animatedLineRef} />
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
+        </View>
     );
-  };
-  
-  export default Tabs;
+};
+
+export default Tabs;
