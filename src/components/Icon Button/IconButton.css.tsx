81cd9871cec0fd8fa8ff4514c982bbd6180c302c
jlr03 <jhonlla975@gmail.com>
2024-02-23 10:38:20 -0500

    refactor(component/layout-button): the IconButtonLayout component is refactored

---
+++ src/components/Icon Button/IconButton.css.tsx
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    marginLeft: 10,
+    marginTop: 10,
+  },
+  circle: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 3,
+  },
+  textButton: {
+    color: '#313235',
+    marginBottom: 20,
+  },
+});
