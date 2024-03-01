620f5e0972339a253cb729b70e194c392a439113
jlr03 <jhonlla975@gmail.com>
2024-03-01 08:47:06 -0500

    cleaning of unnecessary imports

--- src/components/LayoutButton/IconButtonLayout.tsx
+++ src/components/LayoutButton/IconButtonLayout.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
   View,
   Pressable,
-  Text,
   ScrollView,
 } from 'react-native';
 import { Text2 } from '../Text/Text';
