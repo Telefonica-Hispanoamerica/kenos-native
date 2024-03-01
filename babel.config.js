403157b0a953bf6066f2ca79a4a0253ad34be297
Jose Luis Ospino <joseluis.ospinocueva@amaris.com>
2024-03-01 14:08:11 -0500

    feat(componenets/circular-stepper): finish circular stepper

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,5 @@ module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
   plugins: [
     ["babel-plugin-react-docgen-typescript", { exclude: "node_modules" }],
-    'react-native-reanimated/plugin',
   ],
 };
