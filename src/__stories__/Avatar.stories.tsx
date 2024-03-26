8a00e15d9b4255219eff4db7f5b96abef568fda6
jlr03 <jhon_tok35@hotmail.com>
2024-03-26 09:44:38 -0500

    fix(component/avatar): minor styling fixes

--- src/__stories__/Avatar.stories.tsx
+++ src/__stories__/Avatar.stories.tsx
@@ -2,15 +2,24 @@ import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import Avatar from '../components/Avatar/Avatar';
 import { View } from 'react-native';
+import { IconUserAccountRegular } from '../kenos-icons';
 
 export default {
   title: 'Avatar',
   component: Avatar,
 } as ComponentMeta<typeof Avatar>;
 
+
+
 export const Basic: ComponentStory<typeof Avatar> = args => (
-  <View style={{ padding: 16 }}>
-    <Avatar {...args} />
+  <View style={{ padding: 16, gap: 16}}>
+    <Avatar {...args} size={64}/>
+    <Avatar size={args.size} badge={false} src={args.src}/>
+    <Avatar Icon={IconUserAccountRegular} size={args.size} inverse={false} />
+    <Avatar Icon={IconUserAccountRegular} size={args.size} inverse/>
+    <Avatar initials='andrea' size={args.size} inverse/>
+    <Avatar initials='andrea' size={args.size} inverse={false}/>
+
   </View>
 );
 
